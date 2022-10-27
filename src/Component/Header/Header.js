2009040fbf9e56e301da803a7812419fac314052
Mirhasankhan <mirhasan000034@gmail.com>
2022-10-28 00:53:22 +0600

    adding headers

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import './Header.css';
 
 const Header = () => {
     return (
-        <div>
-            <h2>this is header</h2>
+        <div style={{ 
+            backgroundImage: `url("https://weareipig.com/wp-content/uploads/2020/08/Tottenham-Hotspur-Stadium-Glass-Skybridge-as-seen-on-Dezeen.jpg")`,
+            height: '600px', marginBottom: '30px', color: 'Orange', textAlign: 'center' 
+          }}>
+            <h2>Premiere League Footabll</h2>
+            <h2>Liverpool vs Manchester City</h2>
         </div>
     );
 };
--- src/Component/PlayingTeam/PlayingTeam.js
+++ src/Component/PlayingTeam/PlayingTeam.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import './PlayingTeam.css';
 
 const PlayingTeam = () => {
     return (
-        <div>
-            <h3 style={{marginLeft: '140px'}}>Today's Playing XI</h3>
+        <div className="playing-container">
+            <h3
+                style={{ marginLeft: '140px', border: '2px solid blue', padding: '20px', width: '200px', backgroundColor: 'green', color: 'white', borderRadius: '20px' }}>
+                Today's Playing XI
+            </h3>
         </div>
     );
 };
